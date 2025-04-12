3360011a916af9aef79669fdf8f6b6c0d551010b
Your Name <34923485+Haste171@users.noreply.github.com>
2025-04-12 17:37:54 -0500

    hard code pfp and name

--- services/webhook_agent.py
+++ services/webhook_agent.py
@@ -11,7 +11,10 @@ class DiscordWebhookAgent:
 
     def send_notification(self, embeds: List[Dict]):
         data = {
-            "embeds": embeds
+            "embeds": embeds,
+            "username": "siege spider",
+            "avatar_url": "https://cdn.discordapp.com/avatars/1356111768694423704/01d58b63afdb8ada6eb8d46214326fb3.webp?size=160",
+            "attachments": []
         }
 
         response = requests.post(self.webhook_url, json=data)
