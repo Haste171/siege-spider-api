4f496711d61b785846b3265366de75346310dd7a
Your Name <34923485+Haste171@users.noreply.github.com>
2025-04-12 01:09:35 -0500

    fix db url naming to allow inclusion of 'postgresql' on heroku

--- alembic/env.py
+++ alembic/env.py
@@ -9,7 +9,7 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
-DB_URL = os.getenv("DATABASE_URL")
+DB_URL = os.getenv("DB_URL")
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
--- database/handler.py
+++ database/handler.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-DATABASE_URL = os.environ.get("DATABASE_URL")
+DATABASE_URL = os.environ.get("DB_URL")
 
 engine = create_engine(
     DATABASE_URL,
